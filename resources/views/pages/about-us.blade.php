5d9138ac925383b7c46e532dc5c59fea6ac07e65
shynexiii <xiiihab@gmail.com>
2021-04-20 04:35:58 +0100

    Add home and about pages

---
+++ resources/views/pages/about-us.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>About Us | Hello City</title>
+</head>
+<body>
+    <p>Built with &hearts; by Shynexiii.</p>
+    <a href="/">Return to Home page</a>
+
+    <footer>
+        <p>&copy; Copyright {{ date('Y') }}</p>
+    </footer>
+</body>
+</html>
