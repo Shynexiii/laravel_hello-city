d8de76b416b25bf7bad82a4de0af1fd233e1c477
shynexiii <xiiihab@gmail.com>
2021-04-20 06:05:17 +0100

    Removing duplication by using app layout

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-    <title>About Us | Hello City</title>
-</head>
-<body>
+@section('content')
     <p>Built with &hearts; by Shynexiii.</p>
     <a href="/">Return to Home page</a>
-
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }}</p>
-    </footer>
-</body>
-</html>
+@endsection
