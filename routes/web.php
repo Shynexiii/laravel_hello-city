1de5414f33092b89411ed94099c004ebe723b67d
shynexiii <xiiihab@gmail.com>
2021-04-23 18:22:49 +0100

    Make use of Route::view instead of Route::get

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,4 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function(){
-    return view('pages.home');
-})->name('app_home');
-
-
-Route::get('about-us', function(){
-    return view('pages.about-us');
-})->name('app_about_us');
+Route::view('/', 'pages.home')->name('app_home');
+Route::view('about-us', 'pages.about-us')->name('app_about_us');
